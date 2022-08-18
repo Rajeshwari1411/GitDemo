27def9c9af4cd6992e6986689ecc1ca816308a59
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-18 17:05:23 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,17 @@
 ﻿using System;
-public class Exercise7
+public class Exercise8
 {
     public static void Main()
     {
-        int num;
+        int x;
 
-        Console.WriteLine("Enter a digit: ");
-        num = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter a number: ");
+        x = Convert.ToInt32(Console.ReadLine());
 
-        
-        Console.Write(num);
-        Console.Write(" ");
-        Console.Write(num);
-        Console.Write(" ");
-        Console.Write(num);
-        Console.Write(" ");
-        Console.Write(num);
-        Console.WriteLine();
-
-       
-        Console.Write(num);
-        Console.Write(num);
-        Console.Write(num);
-        Console.WriteLine(num);
-        Console.WriteLine();
-
-        
-        Console.WriteLine("{0} {0} {0} {0}", num);
-
-     
-        Console.WriteLine("{0}{0}{0}{0}", num);
+        Console.WriteLine("{0}{0}{0}", x);
+        Console.WriteLine("{0} {0}", x);
+        Console.WriteLine("{0} {0}", x);
+        Console.WriteLine("{0} {0}", x);
+        Console.WriteLine("{0}{0}{0}", x);
     }
 }
