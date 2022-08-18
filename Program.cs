02bdead0b383f471ca9d376b3990dc6720292007
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-18 17:00:35 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
-using System.IO;
-public class Exercise4
+public class Exercise5
 {
     public static void Main()
     {
-        double number1, number2, number3, number4;
+        int number1, number2, number3;
 
-        Console.Write("Enter the First number: ");
-        number1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter first number - ");
+        number1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the Second number: ");
-        number2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter second number - ");
+        number2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the third number: ");
-        number3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter third number - ");
+        number3 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the fourth number: ");
-        number4 = Convert.ToDouble(Console.ReadLine());
-
-        double result = (number1 + number2 + number3 + number4) / 4;
-        Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",
-     number1, number2, number3, number4, result);
+        Console.Write("Result of specified numbers {0}, {1} and {2}, (x+y)·z is {3} and x·y + y·z is {4}\n\n",
+            number1, number2, number3, ((number1 + number2) * number3), (number1 * number2 + number2 * number3));
     }
 }
