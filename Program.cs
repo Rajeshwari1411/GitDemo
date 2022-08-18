0b91a7aad06eeb9a0b7f1d8245721b33b08bc730
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-18 17:07:06 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,12 @@
 ﻿using System;
-class Program
+class Exercise14
 {
     static void Main()
     {
-        double m1 = 200;
-        Console.WriteLine("Meter   ::    Kilometer");
-        double k1 = Exercise13.cMtK(m1);
-        Console.WriteLine("{0}     ::    {1}", m1, k1);
-        double m4 = 3107;
-        double k4 = Exercise13.cMtK(m4);
-        Console.WriteLine("{0}   ::     {1}", m4, k4);
-        double k3 = 5.1;
-        Console.WriteLine();
-        Console.WriteLine("Kilometer     ::     Meter");
-        double m3 = Exercise13.cKtM(k3);
-        Console.WriteLine("{0}           ::     {1}", k3, m3);
-        double k2 = 3.219;
-        double m2 = Exercise13.cKtM(k2);
-        Console.WriteLine("{0}         ::    {1}", k2, m2);
-        Console.Read();
+        float celsius = 40;
+        Console.WriteLine("Temperature in celsius is: " + celsius);
+        float fahrenheit = ((celsius * 9) / 5) + 32;
+        Console.WriteLine("Temperature in Fahrenheit is: " + fahrenheit);
+        Console.ReadKey();
     }
 }
-
-public static class Exercise13
-{
-    public static double cMtK(double meters)
-    {
-        return meters / 1000;
-    }
-
-    public static double cKtM(double kilometers)
-    {
-        return kilometers * 1000;
-    }
-}
-
-
