d0a6eaa8c4738978d01c34778f629ae3ae53eaae
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-18 16:59:55 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿using System;
-public class Exercise3
+using System.IO;
+public class Exercise4
 {
     public static void Main()
     {
-        Console.Write("Enter a number: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
+        double number1, number2, number3, number4;
 
-        Console.Write("Enter another number: ");
-        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the First number: ");
+        number1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
-        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
-        Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
-        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
-        Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
+        Console.Write("Enter the Second number: ");
+        number2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        number3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the fourth number: ");
+        number4 = Convert.ToDouble(Console.ReadLine());
+
+        double result = (number1 + number2 + number3 + number4) / 4;
+        Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",
+     number1, number2, number3, number4, result);
     }
 }
