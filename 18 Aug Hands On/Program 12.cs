01ba20ab3ffefda658f19036ec6421830dbcecd7
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-20 11:00:02 +0530

    Add files via upload

---
+++ 18 Aug Hands On/Program 12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Exercise12
+{
+
+    
+    public static void Main(string[] args)
+    {
+        string s1 = "";
+
+        // or declare String s2.Empty;
+        string s2 = "";
+
+        string s3 = null;
+
+        // for String value Geeks, return true
+        bool b1 = string.IsNullOrEmpty(s1);
+
+        // For String value Empty or "", return true
+        bool b2 = string.IsNullOrEmpty(s2);
+
+        // For String value null, return true
+        bool b3 = string.IsNullOrEmpty(s3);
+
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+        Console.WriteLine(b3);
+    }
+}
