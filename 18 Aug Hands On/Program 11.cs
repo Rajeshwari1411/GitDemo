ef469c433f7967e24d2f453c7cd32b4759779e41
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-20 10:52:47 +0530

    Add files via upload

---
+++ 18 Aug Hands On/Program 11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Exercise11
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput first integer:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input second integer:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Check if both are positive:");
+        Console.WriteLine((x > 0 && y > 0) );
+    }
+}
