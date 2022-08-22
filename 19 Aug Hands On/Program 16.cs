3e0ed32ba46ff28ff840cde351b77a3f86160d2c
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-22 19:09:30 +0530

    Add files via upload

---
+++ 19 Aug Hands On/Program 16.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    internal class Program16
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string str = " ";
+            int sum = 100;
+            for (i = 0; i < 4; i++)
+            {
+                str = Console.ReadLine();
+                if (str.Equals("HIT"))
+                {
+                    sum += 10;
+                }
+                else if (str.Equals("MISS"))
+                {
+                    sum -= 20;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
