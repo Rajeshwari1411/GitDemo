ac1f5ba54d2649bdfe11faa5208845987628f8e7
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-22 06:23:26 +0530

    Add files via upload

---
+++ 19 Aug Hands On/Program 14.cs
@@ -0,0 +1,28 @@
+﻿public class GFG
+{
+
+
+    public static void Main(String[] args)
+    {
+        int nitem = 5;
+        switch (nitem)
+        {
+
+            case 1:
+                Console.WriteLine("case 1");
+                break;
+
+            case 5:
+                Console.WriteLine("case 5");
+                break;
+
+            case 9:
+                Console.WriteLine("case 9");
+                break;
+
+            default:
+                Console.WriteLine("No match found");
+                break;
+        }
+    }
+}
