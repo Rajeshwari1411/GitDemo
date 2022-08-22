0fbcbbfc0232bd62e2d97f430d327896e8f6c810
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-22 19:05:22 +0530

    Add files via upload

---
+++ 19 Aug Hands On/Program 15.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    internal class Program15
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j = 25;
+
+            for (i = 0, j = 25; i < 25 && j >= 0; i++, j--)
+            {
+                if (i > j)
+                {
+                    Console.WriteLine("Crossed Over");
+                    break;
+                }
+                Console.WriteLine("{0} {1}", i, j);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
