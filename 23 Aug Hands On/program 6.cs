ccb421d56596c75afa0d8a34949a4d3772f33c77
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-23 18:37:32 +0530

    Add files via upload

---
+++ 23 Aug Hands On/program 6.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+namespace Exercise
+{
+    enum deptnames
+    {
+        Purchase = 1,
+        Sales = 2,
+        Training = 3,
+        Accounts = 4
+    }
+    public class Program6
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(deptnames.Training);
+
+            int deptno = (int)deptnames.Training;
+
+            Console.WriteLine("\nDept No: " + deptno);
+
+            Console.Read();
+        }
+
+    }
+}
