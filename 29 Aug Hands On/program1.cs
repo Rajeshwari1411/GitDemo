b9f0b6e2a0d9d03106c5f210cae24cc07f7861f4
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-08-29 19:01:41 +0530

    Add files via upload

---
+++ 29 Aug Hands On/program1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+
+namespace aug29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Userstack<string> u = new Userstack<string>();
+            u.Push("Raj");
+            u.Push("Rajiii");
+            u.Push("harini");
+            Console.WriteLine("Popped element");
+            u.Pop();
+            Console.WriteLine("Elements at present");
+            u.printData();
+            Console.WriteLine("Top element at present");
+            string k = u.Peek();
+            Console.WriteLine(k);
+            Console.ReadLine();
+
+
+        }
+
+    }
+    class Userstack<T>
+    {
+        internal static int max = 1000;
+        T[] stack = new T[max];
+        int top = -1;
+        public void Push(T str)
+        {
+            if (top >= 1000)
+            {
+                Console.WriteLine("can't push");
+            }
+            else
+            {
+                stack[++top] = str;
+            }
+        }
+        public void Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("No elements there to pop");
+            }
+            else
+            {
+                T value = stack[top--];
+                Console.WriteLine(value);
+            }
+        }
+        public T Peek()
+        {
+            if (top < 0)
+            {
+                return default(T);
+            }
+            else
+            {
+                T value = stack[top];
+                return value;
+            }
+        }
+        public void printData()
+        {
+            for (int i = top; i >= 0; i--)
+            {
+                Console.WriteLine(stack[i]);
+            }
+        }
+    }
+}
