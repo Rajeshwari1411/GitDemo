20e37144b3946a9b1b97d4b91b365d248b1e2672
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-09-06 14:08:48 +0530

    Add files via upload

---
+++ 05 Sep Hnads On/Cart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library
+{
+    internal class Cart:Book
+    {
+        private int _custid;
+
+        public int CustomerId
+        {
+            get { return _custid; }
+            set { _custid = value; }
+        }
+        private int _bookingid;
+
+        public int BookingID
+        {
+            get { return _bookingid; }
+            set { _bookingid = value; }
+        }
+
+        static int bookcount;
+        public void BookCount()
+        {
+            bookcount = bookcount + 1;
+            Console.WriteLine("Count of Books in Cart: " + bookcount);
+        }
+
+        public void AddToCart(int custid, int bookid, int price)
+        {
+            this.CustomerId = custid;
+            this.BookingID = bookid;
+            this.Price = price;
+        }
+
+    }
+}
