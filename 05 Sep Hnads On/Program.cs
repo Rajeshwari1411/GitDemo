65a302426145ea34657067223fb9b050477b2372
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-09-07 08:36:40 +0530

    Add files via upload

--- 05 Sep Hnads On/Program.cs
+++ 05 Sep Hnads On/Program.cs
@@ -3,29 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Class_Library;
 
-namespace Sep_05_programs
+namespace _5_sep_11_pro
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //BookCategory bcat = new BookCategory();
-            //Console.WriteLine("Book Category id : {0}, Book Category Name : {1}, BookCategory Description : {2}", bcat.CategoryID, bcat.CategoryName,bcat.Description);
+            Cart c = new Cart();
+            c.AcceptData();
+            c.AcceptData();
+            List<Book> Blist = new List<Book>();
+            Blist = c.BooksList();
 
-            //BookCategory bcat2 = new BookCategory(1, "Pride and prejudice" ,"Classics type");
-            //Console.WriteLine("Book Category id : {0}, Book Category Name : {1}, BookCategory Description : {2}", bcat2.CategoryID, bcat2.CategoryName, bcat2.Description);
-
-            Book b = new Book();
-            Console.WriteLine("Book  id : {0}, Book Name : {1}, Book Description : {2}", b.BookID, b.BookName, b.Price);
-
-            Book b2 = new Book(2, "Strange Planet", 500);
-            Console.WriteLine("Book  id : {0}, Book Name : {1}, Book Description : {2}", b2.BookID, b2.BookName, b2.Price);
-
-
-
-            Console.ReadKey();
+            foreach (var i in Blist)
+            {
+                Console.WriteLine("Book ID: " + i.BookID);
+                Console.WriteLine("Book Name: " + i.BookName);
+                Console.WriteLine("Book Price: " + i.BookPrice);
+                c.BookCount();
+            }
+            Console.ReadLine();
         }
     }
+    
 }
