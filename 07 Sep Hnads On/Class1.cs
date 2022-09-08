fa8f4d072a38528d11839b8eceab2458b6e745f8
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-09-08 06:04:25 +0530

    Add files via upload

---
+++ 07 Sep Hnads On/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sep_7
+{
+    public delegate void CapsDelegate();
+    public delegate void stren(string s);
+    public delegate void stren2(int i, int j);
+    public delegate void stren3(Products p);
+    public delegate int stren4(DateTime t);
+    public class strin
+    {
+        public void stringlen(string s)
+        {
+            Console.WriteLine(s.Length);
+
+        }
+        public void greet(string s)
+        {
+            Console.WriteLine($"Hello {s}");
+        }
+    }
+    public class integer
+    {
+        public void Add(int i, int j)
+        {
+            Console.WriteLine($"Addition is {i + j}");
+        }
+        public void Sub(int i, int j)
+        {
+            if (i > j)
+            {
+                Console.WriteLine($"Subtraction is {i - j}");
+            }
+            else
+            {
+                Console.WriteLine($"Subtraction is {j - i}");
+            }
+        }
+        public void Multiply(int i, int j)
+        {
+            Console.WriteLine($"Multiplication is {j * i}");
+        }
+
+    }
+    public class Products
+    {
+        public int PId
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public void Display(Products p)
+        {
+            Console.WriteLine($"Product id is {p.PId} and name is {p.Name}");
+        }
+    }
+    public class Employee
+    {
+        private DateTime _joining;
+        public DateTime Joining
+        {
+            get { return _joining; }
+            set { _joining = value; }
+
+        }
+        public int Expyrs(DateTime ji)
+        {
+            int p = DateTime.Now.Year - ji.Year;
+            return p;
+        }
+    }
+    public class Capitalize
+    {
+        public void FirstCharToUpper()
+        {
+            string str = "capitalizethis";
+
+            if (str.Length == 0)
+            {
+                Console.WriteLine("Empty String");
+            }
+            else if (str.Length == 1)
+            {
+                Console.WriteLine(char.ToUpper(str[0]));
+            }
+            else
+            {
+                Console.WriteLine(char.ToUpper(str[0]) + str.Substring(1));
+            }
+        }
+    }
+
+}
---
+++ 07 Sep Hnads On/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using sep_7;
+
+namespace _7_sep_hands_on
+{
+    internal class Program
+    {
+        public delegate string stren5(string spo, string spo1);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ques 01");
+            Console.WriteLine("Enter the string");
+            string m = Console.ReadLine();
+            strin s1 = new strin();
+            stren s2 = new stren(s1.stringlen);
+            s2(m);
+            Console.WriteLine("Question 2");
+            Console.WriteLine("Enter username");
+            string g = Console.ReadLine();
+            stren s3 = new stren(s1.greet);
+            s3(g);
+            Console.WriteLine("Question 3");
+            Console.WriteLine("Enter the two numbers");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+            integer k = new integer();
+            stren2[] d3 = new stren2[3] { k.Add, k.Sub, k.Multiply };
+            stren2 d4 = (stren2)MulticastDelegate.Combine(d3);
+            d4(i, j);
+            Console.WriteLine("Question 4");
+            Console.WriteLine("Enter productId and product name");
+            Products p = new Products();
+            p.PId = Convert.ToInt32(Console.ReadLine());
+            p.Name = Console.ReadLine();
+            stren3 p1 = new stren3(p.Display);
+            p1(p);
+            Console.WriteLine("Question 5");
+            Console.WriteLine("Enter the year,Month,date");
+            int yr = Convert.ToInt32(Console.ReadLine());
+            int mon = Convert.ToInt32(Console.ReadLine());
+            int dt = Convert.ToInt32(Console.ReadLine());
+            Employee emp = new Employee();
+            DateTime t = new DateTime(yr, mon, dt);
+            emp.Joining = t;
+            stren4 se = new stren4(emp.Expyrs);
+            int poiu = se(emp.Joining);
+            Console.WriteLine($"Years of exp {poiu}");
+            Console.WriteLine("Question 6");
+            Console.WriteLine("Enter strings to Concatenate");
+            string raj = Console.ReadLine();
+            string raji = Console.ReadLine();
+            stren5 dpo = delegate (string good, string nit)
+            {
+                return good + nit;
+            };
+            string sod = dpo(raj, raji);
+            Console.WriteLine($"Concatenated string is {sod}");
+            Console.ReadLine();
+
+            Console.WriteLine("=================================================");
+            Console.WriteLine("Question 7");
+            Capitalize cap = new Capitalize();
+            CapsDelegate c = new CapsDelegate(cap.FirstCharToUpper);
+            c();
+        }
+    }
+
+}
+    
+
