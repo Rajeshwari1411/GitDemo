13ea81aa812d927554adb66022128e556ec0051b
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-10-13 10:44:25 +0530

    Add files via upload

---
+++ 29 sep handson/data.cs
@@ -0,0 +1,12 @@
+﻿namespace data
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class book
+    {
+        public int book_id { get; set; }
+        public string book_name { get; set; }
+        public string book_author { get; set; }
+    }
+}
---
+++ 29 sep handson/loan.cs
@@ -0,0 +1,14 @@
+﻿namespace data
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class loan
+    {
+        public int loanid { get; set; }
+        public string loanname { get; set; }
+        public string amount { get; set; }
+        public string duration { get; set; }
+        public string rateofinterest { get; set; }
+    }
+}
